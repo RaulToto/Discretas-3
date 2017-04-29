67a5d387bb5d69249fb023d215b4487877d36c91
RaulToto <djbrexman@gmail.com>
2017-04-28 23:46:10 -0500

    upload euclides  algorithm

---
+++ Euclides_Algorithm/euclides.cpp
@@ -0,0 +1,6 @@
+#include "euclides.h"
+
+Euclides::Euclides()
+{
+
+}
---
+++ Euclides_Algorithm/euclides.h
@@ -0,0 +1,11 @@
+#ifndef EUCLIDES_H
+#define EUCLIDES_H
+
+
+class Euclides
+{
+public:
+    Euclides();
+};
+
+#endif // EUCLIDES_H
